f685fd5b9b9eb6e05393d7d0c517926b6462d287
n1tr0xs <36129181+NiTroX1221@users.noreply.github.com>
2020-03-24 19:54:20 +0300

    Last console release

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 '''
 
+from datetime import datetime as dt
 
 # start week Monday date
 year = 2020
@@ -9,17 +10,17 @@ month = 2
 day = 3
 
 
-def main():
-    pass
+def main(year, month, day, types=('первая', 'вторая')):
+    start = dt(year=year, month=month, day=day)
+    today = dt.now().date()
+    delta = today-start
+    delta -= delta % 7 # setting day to Monday
 
-def set_start(year, month, day):
-    '''Return date naming "start" - base week Monday
-
-    '''
-    pass
-
-
-def week_type(start, types=('первая', 'вторая')):
-    pass
+    if not delta % 14:
+        return types[0]
+    if not delta % 7:
+        return types[1]
+    return 'Some else'
+    
 
 main()
