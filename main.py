61695ff6d8fa66db4abb0071f696fbe0e1502cce
n1tr0xs <36129181+NiTroX1221@users.noreply.github.com>
2020-03-24 18:27:10 +0300

    Prog skelet created

---
+++ main.py
@@ -0,0 +1,25 @@
+'''This program determines a week type, based on 2-weeks lap.
+
+'''
+
+
+# start week Monday date
+year = 2020
+month = 2
+day = 3
+
+
+def main():
+    pass
+
+def set_start(year, month, day):
+    '''Return date naming "start" - base week Monday
+
+    '''
+    pass
+
+
+def week_type(start, types=('первая', 'вторая')):
+    pass
+
+main()
